b44dc41bc101ce679ed88fcd86898a9fdd61820d
rektide <rektide@voodoowarez.com>
2015-01-21 02:21:17 -0500

    Hack around __malloc_hook dependency by tweaking the configure autoconf bakes out.

--- compileOpus.sh
+++ compileOpus.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+
 # configure libopus
 cd libopus
 if [ ! -f configure ]; then
   # generate and run configuration script
   ./autogen.sh
+  sed -i 's/#define HAVE___MALLOC_HOOK 1/#undef HAVE___MALLOC_HOOK/g' configure
   emconfigure ./configure --prefix="`pwd`" --enable-fixed-point
 fi
 
